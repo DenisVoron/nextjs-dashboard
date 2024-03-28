8103b348eff79048fe542aed48df39c6f9278e84
DenisVoron <Denis.voronko@gmail.com>
2024-03-28 22:42:48 +0200

    add part two

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { string, z } from 'zod';
+import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
